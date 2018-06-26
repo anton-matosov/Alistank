cae9f1c1e14d7355c4d59cf5ff59ab56755b9b3b
Anton Matosov <anton.matosov@gmail.com>
2018-06-25 22:07:05 -0700

    Try to emulate joystick reciever

--- src/index-ble.js
+++ src/index-ble.js
@@ -1,39 +1,68 @@
 
-var noble = require('noble');
 
-noble.on('stateChange', function(state) {
+const bleno = require('bleno');
+const PrimaryService = bleno.PrimaryService;
+const Characteristic = bleno.Characteristic;
+
+
+
+
+// var characteristic = new Characteristic({
+//     uuid: 'fffffffffffffffffffffffffffffff1', // or 'fff1' for 16-bit
+//     properties: [ ... ], // can be a combination of 'read', 'write', 'writeWithoutResponse', 'notify', 'indicate'
+//     secure: [ ... ], // enable security for properties, can be a combination of 'read', 'write', 'writeWithoutResponse', 'notify', 'indicate'
+//     value: null, // optional static value, must be of type Buffer - for read only characteristics
+//     descriptors: [
+//         // see Descriptor for data type
+//     ],
+//     onReadRequest: null, // optional read request handler, function(offset, callback) { ... }
+//     onWriteRequest: null, // optional write request handler, function(data, offset, withoutResponse, callback) { ...}
+//     onSubscribe: null, // optional notify/indicate subscribe handler, function(maxValueSize, updateValueCallback) { ...}
+//     onUnsubscribe: null, // optional notify/indicate unsubscribe handler, function() { ...}
+//     onNotify: null, // optional notify sent handler, function() { ...}
+//     onIndicate: null // optional indicate confirmation received handler, function() { ...}
+// });
+
+// process.env.BLENO_DEVICE_NAME = 'XYZrobot';
+
+bleno.on('stateChange', (state) => {
   if (state === 'poweredOn') {
-    noble.startScanning();
+    var name = 'XYZrobot';
+    var serviceUuids = ['6E400001-B5A3-F393-E0A9-E50E24DCCA9E']
+    
+    bleno.startAdvertising(name, serviceUuids, (error) => {
+      console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
+    });
+
   } else {
-    noble.stopScanning();
+    bleno.stopAdvertising();
   }
 });
 
-noble.on('discover', function(peripheral) {
-  console.log('peripheral discovered (' + peripheral.id +
-              ' with address <' + peripheral.address +  ', ' + peripheral.addressType + '>,' +
-              ' connectable ' + peripheral.connectable + ',' +
-              ' RSSI ' + peripheral.rssi + ':');
-  console.log('\thello my local name is:');
-  console.log('\t\t' + peripheral.advertisement.localName);
-  console.log('\tcan I interest you in any of the following advertised services:');
-  console.log('\t\t' + JSON.stringify(peripheral.advertisement.serviceUuids));
+bleno.on('advertisingStart', function(error) {
 
-  var serviceData = peripheral.advertisement.serviceData;
-  if (serviceData && serviceData.length) {
-    console.log('\there is my service data:');
-    for (var i in serviceData) {
-      console.log('\t\t' + JSON.stringify(serviceData[i].uuid) + ': ' + JSON.stringify(serviceData[i].data.toString('hex')));
-    }
-  }
-  if (peripheral.advertisement.manufacturerData) {
-    console.log('\there is my manufacturer data:');
-    console.log('\t\t' + JSON.stringify(peripheral.advertisement.manufacturerData.toString('hex')));
-  }
-  if (peripheral.advertisement.txPowerLevel !== undefined) {
-    console.log('\tmy TX power level is:');
-    console.log('\t\t' + peripheral.advertisement.txPowerLevel);
-  }
+  if (!error) {
+    var primaryService = new PrimaryService({
+      uuid: '0001', // or 'fff0' for 16-bit
+      characteristics: [
+        new Characteristic({
+          uuid: '0003',
+          properties: ['notify']
+        }),
+        new Characteristic({
+          uuid: '0002',
+          properties: ['writeWithoutResponse', 'write'],
+          onWriteRequest: (data, offset, withoutResponse, callback) => {
+            console.log('Data:', data);
+            
+            callback(this.RESULT_SUCCESS);
+          }
+        })
+      ]
+    });
 
-  console.log();
+    bleno.setServices([primaryService], (error) => {
+      console.log('set services: ' + (error ? 'error ' + error : 'success'));
+    });
+  }
 });
