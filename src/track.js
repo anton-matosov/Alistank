320cd187f9f1e3affe3b5a2015001f52ef879cfd
Anton Matosov <anton.matosov@gmail.com>
2018-06-29 23:04:43 -0700

    Proper values that make RPi to generate servo outputs. BUT there is only 1 pwm output on Rpi

--- src/track.js
+++ src/track.js
@@ -9,15 +9,16 @@ class Track {
   constructor(pin = pwm.pin12) {
     
     this.pin = pin;
-    this.minPWM = 1000;
-    this.pwmRange = 1000;
+    this.minPWM = 100;
+    this.pwmRange = 100;
     this.midPWM = this.minPWM + this.pwmRange / 2;
     this.maxPWM = this.minPWM + this.pwmRange;
     this._outputValue = 0;
     
     this.clockdiv = 192;  //If pwmClock is 192 and pwmRange is 2000 we'll get the PWM frequency = 50 Hz
          /* This is a power-of-two divisor of the base 19.2MHz rate, with a maximum value of 4096 (4.6875kHz) (PWM refresh rate), 8 == 2.4MHz */
-    
+    this.gpioPwmRange = 2000;
+
     this.outputPwm = 0;
 
     this.setup()
@@ -28,7 +29,7 @@ class Track {
 
     rpio.pwmSetClockDivider(this.clockdiv);
     // rpio.pwmSetMode(rpio.PWM_MODE_MS)
-    rpio.pwmSetRange(this.pin, this.maxPWM);
+    rpio.pwmSetRange(this.pin, this.gpioPwmRange);
 
     this.outputValue = 0
   }
